43b9c18cf63d12509197669d3362f63f8cbc569d
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-04-04 21:08:18 +0800

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -195,6 +195,10 @@ class StoryMenuState extends MusicBeatState
 
 		changeWeek();
 		changeDifficulty();
+		
+		#if android
+	        addVirtualPad(FULL, A_B);
+                #end
 
 		super.create();
 	}
